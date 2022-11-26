2a7ddb8eb42355e1919c46bf768563ecca5426dd
EC2 Default User <wakana980830@gmail.com>
2022-11-26 05:07:49 +0000

    テスト進めよう

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -4,14 +4,14 @@ class EndUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  with_options presence: true do
-   validates :email, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-   validates :password, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  end
+  # with_options presence: true do
+  # validates :email, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  # validates :password, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  # end
   
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX,
-       message: '半角英数字で入力して下さい' }
+  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  # validates :password, format: { with: VALID_PASSWORD_REGEX,
+  #     message: '半角英数字で入力して下さい' }
          
   has_many :records, dependent: :destroy
   has_many :pets, dependent: :destroy
