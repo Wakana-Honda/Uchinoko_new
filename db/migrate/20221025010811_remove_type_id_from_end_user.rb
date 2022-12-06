30ebe16dbe3a6461af84f49fc51bd899b800f848
EC2 Default User <wakana980830@gmail.com>
2022-12-06 10:58:09 +0000

    aaa

--- db/migrate/20221025010811_remove_type_id_from_end_user.rb
+++ db/migrate/20221025010811_remove_type_id_from_end_user.rb
@@ -1,5 +1,5 @@
 class RemoveTypeIdFromEndUser < ActiveRecord::Migration[6.1]
   def change
-    remove_column :end_users, :type_id, :integer
+    # remove_column :end_users, :type_id, :integer
   end
 end
