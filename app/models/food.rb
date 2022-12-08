5cd9d64708ac0a41e25523ccd600b76cbbc068ed
EC2 Default User <wakana980830@gmail.com>
2022-12-08 10:23:41 +0000

    画像サイズ変更

--- app/models/food.rb
+++ app/models/food.rb
@@ -9,14 +9,14 @@ class Food < ApplicationRecord
   # has_many :genres, through: :food_genres, dependent: :destroy
   has_one_attached :food_image
   
-  def get_food_image(width, height)
-   unless food_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpeg')
-    food_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-   end
+   def get_food_image(width, height)
+    unless food_image.attached?
+     file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+     food_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
     food_image.variant(resize:"#{width}x#{height}").processed
-  end
-# "#{width}x#{height}"
+   end
+
 
   def previous
    end_user.foods.where("id < ?", self.id).order("id DESC").first
